c8ae04e5cd2f8ebce84e1f3d4e60ea156a808a59
RW <yingyingwang0312@gmail.com>
2020-07-22 20:17:47 -0400

    mood_route

--- app.py
+++ app.py
@@ -24,20 +24,36 @@ def index():
     # Return the template with the teams list passed in
     return render_template('/index.html')
 
-@app.route('/songs/<title>')
-def songs(title):
-    # change finaldata to matt's final collection
-    songs = db.finaltable.find({"Song_name":{'$regex':title}}).limit(10)
-    return  {'results': [s["Song_name"] for s in songs]}
+# @app.route('/songs/<title>')
+# def songs(title):
+#     # change finaldata to matt's final collection
+#     songs = db.finalmood_clusters.find({"track_name":{'$regex':title}}).limit(10)
+#     return  {'results': [s["track_name"] for s in songs]}
 
-@app.route('/cluster/<title>')
+@app.route('/mood/<mood_cluster>')
+def songs(mood_cluster):
+    # change finaldata to matt's final collection
+    songs = db.final.mood_clusters.find({"cluster":int(mood_cluster)}).limit(10)
+    return  {'results': [s["track_name"] for s in songs]}
+
+@app.route('/similar/<title>')
 def similarsongs(title):
     # change finaldata to matt's final collection
-   
-    record = db.finaltable.find({"Song_name":{'$regex':title}}) # search base ofd partial input
-    cluster=[r["Cluster_name"] for r in record][0]
-    songs = db.finaltable.find({"Cluster_name":cluster}).limit(10)
-    return  {'results': [s["Song_name"] for s in songs]}
+    try:
+        record = db.final.song_clusters.find({"track_name":{'$regex':title.proper()}}) # search base ofd partial input
+        cluster=[r["cluster"] for r in record][0]
+    except Exception as e:
+        try:
+            record= db.final.song_clusters.find({"track_name":{'$regex':title.lower()}})
+            cluster+=[r["cluster"] for r in record][0]
+        except Exception as e:
+            try:
+                record= db.final.song_clusters.find({"track_name":{'$regex':title.upper()}})
+                cluster+=[r["cluster"] for r in record][0]
+            except Exception as e:
+                pass   
+    songs = db.final.song_clusters.find({"cluster":int(cluster)}).limit(10)
+    return  {'results': [s["track_name"] for s in songs]}
 
 
 if __name__ == "__main__":
